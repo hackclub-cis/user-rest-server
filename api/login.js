95d1eca0a567cf71c8ccb60c466f17dd0782c4b7
Arthelon <hsing.daniel@gmail.com>
2016-10-16 12:17:48 +0800

    Added api annotations

--- api/login.js
+++ api/login.js
@@ -1,6 +1,15 @@
 const router = require("express").Router()
 const User = require("../User")
 
+/**
+* @api {post} /login Authenticate the user
+*
+* @apiParam {String} username   User's username
+* @apiParam {String} password   User's password
+*
+* @apiReturn {Boolean} success     Boolean indicator for login success
+* @apiReturn {String} message      Success/error message
+*/
 router.post("/", (req, res) => {
     User.find(req.body, (err, user) => {
         if (err)
--- api/user.js
+++ api/user.js
@@ -2,8 +2,11 @@ const userRouter = require("express").Router()
 const User = require("../User")
 
 /**
-* /api/users GET
-* @param
+* @api {get} /api/users Retrieve list of users
+*
+* @apiReturn {Boolean} success     Boolean indicator for success of retrieval
+* @apiReturn {String} message      Success/error message
+* @apiSuccess {Object} data        Array of user objects
 */
 userRouter.get("/", (req, res, next) => {
     User.find({}, (err, users) => {
@@ -18,6 +21,17 @@ userRouter.get("/", (req, res, next) => {
         }
     })
 })
+/**
+* @api {post} /api/users Create a new user
+*
+* @apiParam {String} username   User's username
+* @apiParam {String} password   User's password
+* @apiParam {String} email      User's email address
+*
+* @apiReturn {Boolean} success     Boolean indicator for success of retrieval
+* @apiReturn {String} message      Success/error message
+* @apiSuccess {Object} data        Newly created user object
+*/
 userRouter.post("/", (req, res, next) => {
     User.create(req.body, (err, user) => {
         if (err)
